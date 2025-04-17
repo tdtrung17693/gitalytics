cd64f91816f4e82f2a238bb29e148d04712e85fa
Trung Tran <trandinhtrung176@gmail.com>
2025-04-17 18:40:22 +0700

    🐛: typo in application name

--- src/pages/_home.tsx
+++ src/pages/_home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8 min-h-screen">
       <header className="mb-12 text-center pt-16">
-        <h1 className="text-4xl font-bold tracking-tight mb-2">Gitlytics</h1>
+        <h1 className="text-4xl font-bold tracking-tight mb-2">Gitalytics</h1>
         <p className="text-lg text-muted-foreground max-w-2xl mx-auto mb-2">
           Compare GitHub repositories to find the right open-source library for
           your project
